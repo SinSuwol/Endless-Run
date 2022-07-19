98730f7e9ec0f2d44fedec0553abae905f4a139b
SinSuwol <ddd5912@naver.com>
2022-07-19 17:47:01 +0900

    Endless Run
    2022 07 19

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -12,8 +12,10 @@ public class GameManager : MonoBehaviour
     public float speed;
     public bool state;
     public int coin;
+    public int hat;
+    public int rod;
 
-    void Start()
+    void Awake()
     {
         Load();
 
@@ -29,12 +31,14 @@ public class GameManager : MonoBehaviour
     public void Save()
     {
         PlayerPrefs.SetInt("Coin", coin);
+        PlayerPrefs.SetInt("Hat", hat);
+        PlayerPrefs.SetInt("Rod", rod);
     }
 
     public void Load()
     {
         coin = PlayerPrefs.GetInt("Coin");
+        hat = PlayerPrefs.GetInt("Hat");
+        rod = PlayerPrefs.GetInt("Rod");
     }
-
-
 }
